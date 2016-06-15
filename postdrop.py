ec4bde104bbebc9dc3c2b0297edf521f9696de08
William Stevens <contact@wastevensv.com>
2016-06-15 13:09:54 -0400

    Added ability to read private notes.

--- postdrop.py
+++ postdrop.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
+
 import onetimepass as otp
 import requests
 import util
 
 from sys import argv
 import json
+
+quiet = False
 
 with open("primary.key") as f:
   primary_key = f.read().strip()
@@ -17,11 +21,23 @@ with open("hostname.key") as f:
 def get_auth():
   return util.md5(primary_key + str(otp.get_totp(secret=otp_secret)))
 
-def read_public_note(shorturl):
+def read_note(shorturl):
+  if not quiet: print("Retrieving note: ",shorturl)
   r = requests.get(hostname+"/note/"+shorturl)
   if r.status_code == requests.codes.ok:
-    return r.text
+    return r.json()
+  elif r.status_code == requests.codes.forbidden:
+    return read_private_note(shorturl)
   else:
-    return r.status_code
+    return None, r.status_code
 
-print(read_public_note(argv[1]))
+def read_private_note(shorturl):
+  if not quiet: print("Retrieving private note: ",shorturl)
+  payload = {'auth': get_auth()}
+  r = requests.post(hostname+"/note/"+shorturl, json=payload)
+  if r.status_code == requests.codes.ok:
+    return r.json()
+  else:
+    return None, r.status_code
+
+print(read_note(argv[1]))
